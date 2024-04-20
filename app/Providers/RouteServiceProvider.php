74dc78e16b119c251b6d12cd4c9e1c60d16c4a49
rafaelperez01 <rafaelperez7461@hotmail.com>
2024-04-20 12:31:20 +0200

    - add Adminlte librery

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,10 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('web') // add to middleware , 'auth'
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
         });
     }
 }
